1347381df5c3c7b7f498819c5cac2b1dde63b938
lovishlm <miglanilovish5224@gmail.com>
2026-05-03 23:13:09 +0530

    fix api url for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "team-task-manager-1.up.railway.app"
 });
 
 API.interceptors.request.use((req) => {
